beab7e8fa5783ed68332c82fd7410e4ea7ff22b3
Ishara Udayamali <isharaudayamali220@gmail.com>
2025-11-05 22:15:51 +0530

    change server port from 4000 to 3000

--- server/app.js
+++ server/app.js
@@ -20,7 +20,7 @@ app.get('/', function (req, res) {
 
 // app.use('/api/products', productRoute);
 
-const port = 4000;
+const port = 3000;
 app.listen(port, function () {
     console.log(`Server is running on port ${port}...`);
 });

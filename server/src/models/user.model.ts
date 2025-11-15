a3c1aa9d3f785442c4bd04ca6d81ce410b169c46
Ishara Udayamali <isharaudayamali220@gmail.com>
2025-11-15 23:50:51 +0530

    user model updated

--- server/src/models/user.model.ts
+++ server/src/models/user.model.ts
@@ -3,6 +3,8 @@ import { Schema, model, Document } from "mongoose";
 export interface IUser extends Document {
   _id: string;
   username: string;
+  phone: string;
+  address: string;
   email?: string;
   password: string;
   role: "user" | "admin";
@@ -15,6 +17,14 @@ const userSchema = new Schema<IUser>(
       type: String,
       required: true,
       unique: true
+    },
+    phone:{
+      type: String,
+      required: true
+    },
+    address:{
+      type: String,
+      required: true
     },
     email: {
       type: String,

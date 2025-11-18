7fdae4c0664411f9b31919b32476a68cdb094fea
Ishara Udayamali <isharaudayamali220@gmail.com>
2025-11-18 16:23:03 +0530

    order.route.ts done

---
+++ server/src/routes/order.route.ts
@@ -0,0 +1,29 @@
+import { type Router } from "express";
+import { OrderController } from "../controllers/order.controller";
+
+export default class OrderRoutes {
+    private OrderController: OrderController;
+
+    constructor(private router: Router) {
+        this.OrderController = new OrderController();
+        this.configureRoutes();
+    }
+
+    private configureRoutes() {
+
+        // Create api/order/
+        this.router.post('/order', this.OrderController.createOrder);
+
+        // GET api/order/
+        this.router.get('/order', this.OrderController.getAllOrders);
+
+        // GET api/order/:id
+        this.router.get('/order/:id', this.OrderController.getOrderById);
+
+        // PUT api/order/:id
+        this.router.put('/order/:id', this.OrderController.updateOrderById);
+
+        // DELETE api/order/:id
+        this.router.delete('/order/:id', this.OrderController.deleteOrderById);
+    }
+}
